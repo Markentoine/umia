429b67388b0ea3500d7b99b4f5a868d58a5df30a
Jerome Toulouse <monsieur.toulouse@gmail.com>
2019-03-29 12:39:31 +0100

    Test continue.

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,9 @@
     document.addEventListener('DOMContentLoaded', function () {
         const plate = document.getElementById('svg-plate');
         const centerPlate = document.getElementById('svg-center-plate');
-        plate.classList.add('rotate');
-        centerPlate.classList.add('antirotate');
+        setTimeout(() => {
+            plate.classList.add('rotate');
+            centerPlate.classList.add('antirotate');
+        }, 2000);
     });
 }());
