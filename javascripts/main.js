13ea96b183474b16978ea05c69ac87979965b6be
Jerome Toulouse <monsieur.toulouse@gmail.com>
2019-03-30 13:23:44 +0100

    Implement fade in function.

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,9 +2,29 @@
     document.addEventListener('DOMContentLoaded', function () {
         const plate = document.getElementById('svg-plate');
         const centerPlate = document.getElementById('svg-center-plate');
+        const UmiaTitle = document.getElementById('umia-title');
         setTimeout(() => {
             plate.classList.add('rotate');
             centerPlate.classList.add('antirotate');
+            effects.fadeIn(UmiaTitle);
         }, 300);
     });
 }());
+
+const effects = {
+    fadeIn: el => {
+        el.classList.remove('hide');
+        let op = 0;
+        let bl = 5;
+        el.display = 'block';
+        const timer = setInterval(() => {
+            if (op >= 1) {
+                clearInterval(timer);
+            }
+            el.style.opacity = op;
+            el.style.filter = `blur(${bl})px`;
+            op += 0.005;
+            bl -= .025;
+        }, 1);
+    },
+};
