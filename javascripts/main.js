bf80cc1f83064b29b8808bb9ba2f408bd7a6a677
Jerome Toulouse <monsieur.toulouse@gmail.com>
2019-03-31 16:16:15 +0200

    Progressive appearance in hero mobile.

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,10 +3,24 @@
         const plate = document.getElementById('svg-plate');
         const centerPlate = document.getElementById('svg-center-plate');
         const UmiaTitle = document.getElementById('umia-title');
+        const cuisineVoyages = document.getElementById('CuisineVoyages');
+        const iconPhoneCall = document.getElementById('phone-call-out');
+        const telephone = document.getElementById('telephone');
+        const address = document.getElementById('address');
         setTimeout(() => {
             plate.classList.add('rotate');
             centerPlate.classList.add('antirotate');
             effects.fadeIn(UmiaTitle);
+            setTimeout(() => {
+                effects.fadeIn(cuisineVoyages);
+                setTimeout(() => {
+                    effects.fadeIn(iconPhoneCall);
+                    effects.fadeIn(telephone);
+                    setTimeout(() => {
+                        effects.fadeIn(address);
+                    }, 500);
+                }, 500);
+            }, 1000);
         }, 300);
     });
 }());
